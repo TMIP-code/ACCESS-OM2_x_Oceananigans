d42f6b99908d70cbddf5309fed5393be20b3a08b
Benoit Pasquier <briochemc@gmail.com>
2025-12-19 18:06:06 +1100

    Add supergrid extender + dummy plot maker for checking

--- src/ACCESS-OM2_grid.jl
+++ src/ACCESS-OM2_grid.jl
@@ -1,118 +1,135 @@
-@info """
-The supergrid for ACCESS-OM2
-is a NetCDF file with 2x the resolution of the model grid
-It contains the following data:
-ncdump -h /g/data/xp65/public/apps/access_moppy_data/grids/mom1deg.nc
-netcdf mom1deg {
-dimensions:
-        string = 255 ;
-        nxp = 721 ;
-        nyp = 601 ;
-        nx = 720 ;
-        ny = 600 ;
-variables:
-        char tile(string) ;
-                tile:standard_name = "grid_tile_spec" ;
-                tile:tile_spec_version = "0.2" ;
-                tile:geometry = "spherical" ;
-                tile:discretization = "logically_rectangular" ;
-                tile:conformal = "true" ;
-        double x(nyp, nxp) ;
-                x:standard_name = "geographic_longitude" ;
-                x:units = "degree_east" ;
-        double y(nyp, nxp) ;
-                y:standard_name = "geographic_latitude" ;
-                y:units = "degree_north" ;
-        double dx(nyp, nx) ;
-                dx:standard_name = "grid_edge_x_distance" ;
-                dx:units = "meters" ;
-        double dy(ny, nxp) ;
-                dy:standard_name = "grid_edge_y_distance" ;
-                dy:units = "meters" ;
-        double angle_dx(nyp, nxp) ;
-                angle_dx:standard_name = "grid_vertex_x_angle_WRT_geographic_east" ;
-                angle_dx:units = "degrees_east" ;
-        double area(ny, nx) ;
-                area:standard_name = "grid_cell_area" ;
-                area:units = "m2" ;
-}
+# @info """
+# The supergrid for ACCESS-OM2
+# is a NetCDF file with 2x the resolution of the model grid
+# It contains the following data:
+# ncdump -h /g/data/xp65/public/apps/access_moppy_data/grids/mom1deg.nc
+# netcdf mom1deg {
+# dimensions:
+#         string = 255 ;
+#         nxp = 721 ;
+#         nyp = 601 ;
+#         nx = 720 ;
+#         ny = 600 ;
+# variables:
+#         char tile(string) ;
+#                 tile:standard_name = "grid_tile_spec" ;
+#                 tile:tile_spec_version = "0.2" ;
+#                 tile:geometry = "spherical" ;
+#                 tile:discretization = "logically_rectangular" ;
+#                 tile:conformal = "true" ;
+#         double x(nyp, nxp) ;
+#                 x:standard_name = "geographic_longitude" ;
+#                 x:units = "degree_east" ;
+#         double y(nyp, nxp) ;
+#                 y:standard_name = "geographic_latitude" ;
+#                 y:units = "degree_north" ;
+#         double dx(nyp, nx) ;
+#                 dx:standard_name = "grid_edge_x_distance" ;
+#                 dx:units = "meters" ;
+#         double dy(ny, nxp) ;
+#                 dy:standard_name = "grid_edge_y_distance" ;
+#                 dy:units = "meters" ;
+#         double angle_dx(nyp, nxp) ;
+#                 angle_dx:standard_name = "grid_vertex_x_angle_WRT_geographic_east" ;
+#                 angle_dx:units = "degrees_east" ;
+#         double area(ny, nx) ;
+#                 area:standard_name = "grid_cell_area" ;
+#                 area:units = "m2" ;
+# }
 
-julia> supergrid_ds = open_dataset(supergridfile)
-YAXArray Dataset
-Shared Axes:
-None
-Variables with additional axes:
-  Additional Axes:
-  (↓ nx Sampled{Int64} 1:720 ForwardOrdered Regular Points,
-  → ny Sampled{Int64} 1:600 ForwardOrdered Regular Points)
-  Variables:
-  area
+# julia> supergrid_ds = open_dataset(supergridfile)
+# YAXArray Dataset
+# Shared Axes:
+# None
+# Variables with additional axes:
+#   Additional Axes:
+#   (↓ nx Sampled{Int64} 1:720 ForwardOrdered Regular Points,
+#   → ny Sampled{Int64} 1:600 ForwardOrdered Regular Points)
+#   Variables:
+#   area
 
-  Additional Axes:
-  (↓ nxp Sampled{Int64} 1:721 ForwardOrdered Regular Points,
-  → ny Sampled{Int64} 1:600 ForwardOrdered Regular Points)
-  Variables:
-  dy
+#   Additional Axes:
+#   (↓ nxp Sampled{Int64} 1:721 ForwardOrdered Regular Points,
+#   → ny Sampled{Int64} 1:600 ForwardOrdered Regular Points)
+#   Variables:
+#   dy
 
-  Additional Axes:
-  (↓ nx Sampled{Int64} 1:720 ForwardOrdered Regular Points,
-  → nyp Sampled{Int64} 1:601 ForwardOrdered Regular Points)
-  Variables:
-  dx
+#   Additional Axes:
+#   (↓ nx Sampled{Int64} 1:720 ForwardOrdered Regular Points,
+#   → nyp Sampled{Int64} 1:601 ForwardOrdered Regular Points)
+#   Variables:
+#   dx
 
-  Additional Axes:
-  (↓ string Sampled{Int64} 1:255 ForwardOrdered Regular Points)
-  Variables:
-  tile
+#   Additional Axes:
+#   (↓ string Sampled{Int64} 1:255 ForwardOrdered Regular Points)
+#   Variables:
+#   tile
 
-  Additional Axes:
-  (↓ nxp Sampled{Int64} 1:721 ForwardOrdered Regular Points,
-  → nyp Sampled{Int64} 1:601 ForwardOrdered Regular Points)
-  Variables:
-  angle_dx, x, y
+#   Additional Axes:
+#   (↓ nxp Sampled{Int64} 1:721 ForwardOrdered Regular Points,
+#   → nyp Sampled{Int64} 1:601 ForwardOrdered Regular Points)
+#   Variables:
+#   angle_dx, x, y
 
-This file was taken from Oceananigans.jl examples and modified
-to my testing needs.
+# This file was taken from Oceananigans.jl examples and modified
+# to my testing needs.
 
-To run this on Gadi interactively, use
+# To run this on Gadi interactively, use
 
-```
-qsub -I -P y99 -l mem=47GB -l walltime=01:00:00 -l ncpus=12 -l storage=gdata/xp65
-cd /home/561/bp3051/Projects/TMIP/ACCESS-OM2_x_Oceananigans
-julia
-include("src/ACCESS-OM2_grid.jl")
-```
+# ```
+# qsub -I -P y99 -l mem=47GB -l walltime=01:00:00 -l ncpus=12 -l storage=gdata/xp65
+# cd /home/561/bp3051/Projects/TMIP/ACCESS-OM2_x_Oceananigans
+# julia
+# include("src/ACCESS-OM2_grid.jl")
+# ```
 
-And on the GPU queue, use
+# And on the GPU queue, use
 
-```
-qsub -I -P y99 -l mem=47GB -q gpuvolta -l walltime=01:00:00 -l ncpus=12 -l ngpus=1 -l storage=gdata/xp65
-cd /home/561/bp3051/Projects/TMIP/ACCESS-OM2_x_Oceananigans
-julia
-include("src/ACCESS-OM2_grid.jl")
-```
-"""
-using Pkg
-Pkg.activate(".")
-Pkg.instantiate()
+# ```
+# qsub -I -P y99 -l mem=47GB -q gpuvolta -l walltime=01:00:00 -l ncpus=12 -l ngpus=1 -l storage=gdata/xp65
+# cd /home/561/bp3051/Projects/TMIP/ACCESS-OM2_x_Oceananigans
+# julia
+# include("src/ACCESS-OM2_grid.jl")
+# ```
+# """
+# using Pkg
+# Pkg.activate(".")
+# Pkg.instantiate()
 
-using YAXArrays
-using DimensionalData
-using NetCDF
-using Oceananigans
+# using YAXArrays
+# using DimensionalData
+# using NetCDF
+# using Oceananigans
 
 
+# # (from YAXArrays + NetCDF):
 
-# (from YAXArrays + NetCDF):
-
-model = "ACCESS-OM2-1"
-modelsupergridfile = "mom$(split(model, "-")[end])deg.nc"
-supergridfile = joinpath("/g/data/xp65/public/apps/access_moppy_data/grids", modelsupergridfile)
-supergrid_ds = open_dataset(supergridfile)
-supergrid = readcubedata(supergrid_ds)
+# model = "ACCESS-OM2-1"
+# modelsupergridfile = "mom$(split(model, "-")[end])deg.nc"
+# supergridfile = joinpath("/g/data/xp65/public/apps/access_moppy_data/grids", modelsupergridfile)
+# supergrid_ds = open_dataset(supergridfile)
+# supergrid = readcubedata(supergrid_ds)
+@show supergrid
 
 include("tripolargrid_reader.jl")
 
+# Unpack supergrid data
+# TODO: I think best to extract the raw data here
+# instead of passing YAXArrays
+x = supergrid.x.data
+y = supergrid.y.data
+dx = supergrid.dx.data
+dy = supergrid.dy.data
+area = supergrid.area.data
+# TODO: For dimensions, just get the lengths instead of index ranges
+# Not sure this matters but it is a bit more consistent
+# with Nx, Ny, etc. used elsewhere where "N" or "n" means number of points
+nx = length(supergrid.nx.val)
+nxp = length(supergrid.nxp.val)
+ny = length(supergrid.ny.val)
+nyp = length(supergrid.nyp.val)
+
 grid = tripolargrid_from_supergrid(;
-    supergrid = supergrid,
+    x, y, dx, dy, area,
+    nx, nxp, ny, nyp,
 )
--- src/tripolargrid_reader.jl
+++ src/tripolargrid_reader.jl
@@ -72,7 +72,7 @@ function compute_metrics_from_supergrid!(
         nx, ny, dx, dy, area
     )
 
-    for (i, j) in axes(Δxᶜᶜᵃ)
+    for i in axes(Δxᶜᶜᵃ, 1), j in axes(Δxᶜᶜᵃ, 2)
         𝑖, 𝑗 = 2i, 2j
         # For Δx, wrap x indices around with mod1(𝑖 - 2, nx)
         Δxᶜᶜᵃ[i, j] = dx[𝑖 - 1, 𝑗] + dx[𝑖, 𝑗]
@@ -96,22 +96,17 @@ end
 
 function tripolargrid_from_supergrid(
         arch = CPU(), FT::DataType = Float64;
-        # size,
-        # southernmost_latitude = -80,
+        x, y, dx, dy, area,
+        nx, nxp, ny, nyp,
         halosize = (4, 4, 4),
         radius = Oceananigans.defaults.planet_radius,
         z = (0, 1), # Maybe z can be 3D array here?
         # north_poles_latitude = 55,
         # first_pole_longitude = 70,
-        supergrid
     )  # second pole is at longitude `first_pole_longitude + 180ᵒ`
 
-    # supergrid data
-    (; x, y, dx, dy, area, nx, ny, nxp, nyp) = supergrid
-    nx, ny, nxp, nyp = length.((nx, ny, nxp, nyp)) # get sizes instead of ranges
-
     @show southernmost_latitude = minimum(y)
-    @show latitude  = (southernmost_latitude, 90)
+    @show latitude = (southernmost_latitude, 90)
     @show longitude = (-180, 180)
     max_latitudes = maximum(y, dims = 2)
     @show north_poles_latitude, i_north_pole = findmin(max_latitudes)
@@ -374,4 +369,93 @@ function tripolargrid_from_supergrid(
     )
 
     return grid
+end
+
+
+"""
+Extend the supergrid data with one row.
+"""
+function extend_supergrid(; x, y, dx, dy, area, nx, nxp, ny, nyp)
+    ny += 2
+    nyp += 2
+    x = hcat(x, fill(NaN, nxp, 2))
+    y = hcat(y, fill(NaN, nxp, 2))
+    dy = hcat(dy, fill(NaN, nxp, 2))
+    dx = hcat(dx, fill(NaN, nx, 2))
+    area = hcat(area, fill(NaN, nx, 2))
+    for i in 1:nxp, j in 1:2
+        x[i, nyp - 2 + j] = x[nxp - i + 1, nyp - 2 - j]
+        y[i, nyp - 2 + j] = y[nxp - i + 1, nyp - 2 - j]
+        dy[i, ny - 2 + j] = dy[nxp - i + 1, ny - 1 - j]
+    end
+    for i in 1:nx, j in 1:2
+        dx[i, nyp - 2 + j] = dx[nx - i + 1, nyp - 2 - j]
+        area[i, ny - 2 + j] = area[nx - i + 1, ny - 1 - j]
+    end
+    return (; x, y, dx, dy, area, nx, nxp, ny, nyp)
+end
+
+
+"""
+Generate dummy supergrid data for testing.
+"""
+function dummy_supergrid(; nx = 8, ny = 8)
+    nxp = nx + 1
+    nyp = ny + 1
+    x = rand(nxp, nyp)
+    y = rand(nxp, nyp)
+    dx = rand(nx, nyp)
+    dy = rand(nxp, ny)
+    area = rand(nx, ny)
+    return (; x, y, dx, dy, area, nx, nxp, ny, nyp)
+end
+
+"""
+Plot the extended supergrid with markers at key points.
+
+```
+using Pkg
+Pkg.activate(".")
+Pkg.instantiate()
+using CairoMakie
+include("src/tripolargrid_reader.jl")
+supergrid = dummy_supergrid()
+extended_supergrid = extend_supergrid(; supergrid...)
+using CairoMakie
+fig = Figure()
+ax = Axis(fig[1, 1]; aspect = DataAspect())
+plot_extended_supergrid!(ax, extended_supergrid...)
+@show figname = "output/extended_supergrid_plot.png"
+save(figname, fig)
+```
+"""
+function plot_extended_supergrid!(ax, x, y, dx, dy, area, nx, nxp, ny, nyp)
+    ax.xticks = 1:2:nxp
+    ax.yticks = 1:2:nyp
+    ax.xminorticks = 1:nxp
+    ax.yminorticks = 1:nyp
+    ax.xminorgridvisible = true
+    ax.yminorgridvisible = true
+    ax.limits = ((0.7, nxp + 0.3), (0.7, nyp + 0.3))
+    # North pole fold / seam
+    lines!(ax, [1, nxp], [nyp - 2, nyp - 2]; color = :red, linewidth = 2)
+    scatter!(ax, 1, nyp - 2; marker = :circle, color = :red)
+    text!(ax, 1, nyp - 2; text = "P1", color = :red, offset = (+3, +3), align = (:left, :bottom))
+    scatter!(ax, nx ÷ 2 + 1, nyp - 2; marker = :circle, color = :red)
+    text!(ax, nx ÷ 2 + 1, nyp - 2; text = "P2", color = :red, offset = (+3, +3), align = (:left, :bottom))
+    scatter!(ax, nxp, nyp - 2; marker = :circle, color = :red)
+    text!(ax, nxp, nyp - 2; text = "P1′", color = :red, offset = (-3, +3), align = (:right, :bottom))
+    # x y markers
+    xymarkers = [
+        :circle :utriangle
+        :star5 :rect
+    ]
+    for i in 1:2, j in 1:2
+        imarkers = [i, nxp - i + 1]
+        jmarkers = [nyp - 2 + j, nyp - 2 - j]
+        color = [x[imarkers[1], jmarkers[1]], x[imarkers[2], jmarkers[2]]]
+        options = (; colormap = :jet, color, colorrange = (0, 1), strokecolor = :black, strokewidth = 2, markersize = 20)
+        scatter!(ax, imarkers, jmarkers; marker = xymarkers[i, j], options...)
+    end
+    return
 end
