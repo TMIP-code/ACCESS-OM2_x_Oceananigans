8e6c74b6594e59c4c2b6519efea617767be9293b
Benoit Pasquier <briochemc@gmail.com>
2025-12-29 13:33:19 +1100

    WIP diagnose/fix instability

--- src/offline_ACCESS-OM2.jl
+++ src/offline_ACCESS-OM2.jl
@@ -2,7 +2,7 @@
 To run this on Gadi interactively, use
 
 ```
-qsub -I -P y99 -l mem=47GB -l walltime=01:00:00 -l ncpus=12 -l storage=gdata/xp65+scratch/y99
+qsub -I -P y99 -l mem=47GB -l walltime=01:00:00 -l ncpus=12 -l storage=gdata/xp65+gdata/ik11+scratch/y99
 cd /home/561/bp3051/Projects/TMIP/ACCESS-OM2_x_Oceananigans
 julia
 include("src/ACCESS-OM2_grid.jl")
@@ -11,7 +11,7 @@ include("src/ACCESS-OM2_grid.jl")
 And on the GPU queue, use
 
 ```
-qsub -I -P y99 -l mem=47GB -q gpuvolta -l walltime=01:00:00 -l ncpus=12 -l ngpus=1 -l storage=gdata/xp65+scratch/y99
+qsub -I -P y99 -l mem=47GB -q gpuvolta -l walltime=01:00:00 -l ncpus=12 -l ngpus=1 -l storage=gdata/xp65+gdata/ik11+scratch/y99
 cd /home/561/bp3051/Projects/TMIP/ACCESS-OM2_x_Oceananigans
 julia
 include("src/ACCESS-OM2_grid.jl")
@@ -51,6 +51,9 @@ supergridfile = joinpath("/g/data/xp65/public/apps/access_moppy_data/grids", mod
 supergrid_ds = open_dataset(supergridfile)
 @show supergrid_ds
 
+# TODO: Maybe I should only use the supergrid for the locations
+# of center/face/corner points but otherwise use the "standard"
+# grid metrics available from the MOM outputs?
 include("tripolargrid_reader.jl")
 
 # Unpack supergrid data
@@ -78,21 +81,49 @@ supergrid = convert_Fpointpivot_to_Tpointpivot(; supergrid...)
 ####################
 
 experiment = "1deg_jra55_iaf_omip2_cycle6"
+# Here I use the z-coordinate grid (not using time-dependent dht output)
+# TODO: use the dht from zstar coordinate (commented out below)
+# FIXME: This path will need to be updated for different models/experiments
+@show MOM_output_grid_inputdir = "/g/data/ik11/outputs/access-om2/$experiment/output305/ocean/"
+MOM_output_grid_ds = open_dataset(joinpath(MOM_output_grid_inputdir, "ocean_grid.nc"))
+ht = readcubedata(MOM_output_grid_ds.ht).data
+ht = replace(ht, missing => 0.0)
+kmt = readcubedata(MOM_output_grid_ds.kmt).data
+
+@show MOM_input_vgrid_file = "/g/data/ik11/inputs/access-om2/input_20201102/mom_1deg/ocean_vgrid.nc"
+z_ds = open_dataset(MOM_input_vgrid_file)
+z = -reverse(vec(z_ds["zeta"].data[1:2:end]))
+Nz = length(z) - 1
+kbottom = round.(Union{Missing, Int}, Nz .- kmt .+ 1)
+
+@show MOM_input_topo_file = "/g/data/ik11/inputs/access-om2/input_20201102/mom_1deg/topog.nc"
+bottom_ds = open_dataset(MOM_input_topo_file)
+bottom = -readcubedata(bottom_ds["depth"]).data
+bottom = replace(bottom, 9999.0 => 0.0)
+
+# Check if topography matches kmt and ht
+@assert ht == -bottom
+for idx in eachindex(kbottom)
+    k = kbottom[idx]
+    ismissing(kmt[idx]) && continue
+    @assert z[k] ≤ bottom[idx] < z[k + 1]
+end
+@info "z coordinate/grid checks passed."
+
+# Now since I am merging cells on the north fold,
+# I just use the maximum depth on either side of the fold.
+# TODO: This is a hack, so remove once the F-point pivot grid is implemented
+# in Oceananigans!
+Nx, Ny = size(bottom)
+for i in 1:Nx
+    bottom[i, Ny] = max(bottom[i, Ny], bottom[Nx - i + 1, Ny])
+end
+
 time_window = "Jan1960-Dec1979"
 @show inputdir = "/scratch/y99/TMIP/data/$model/$experiment/$time_window"
-dht_ds = open_dataset(joinpath(inputdir, "dht.nc")) # <- (new) cell thickness?
-dht = readcubedata(dht_ds.dht)
-# Start with constant vertical grid as an approximation
-# Chose the deepest water column as the reference
-# TODO: match the vertical grid more closely later
-dht0 = replace(dht.data, NaN => 0.0)
-bottom = -dropdims(sum(dht0, dims = 3), dims = 3) # in MOM z increases downward
-@warn "Setting bottom to +100m for all land poitns"
-bottom[bottom .== 0.0] .= 100 # make sure it's strictly positive for land
-max_dht, imax = findmin(bottom, dims = (1, 2))
-izmax, jzmax = Tuple(imax[1])
-z = -reverse([0; cumsum(dht0[izmax, jzmax, :])])
-Nz = length(z) - 1
+
+# dht_ds = open_dataset(joinpath(inputdir, "dht.nc")) # <- (new) cell thickness?
+# dht = readcubedata(dht_ds.dht)
 
 # TODO: I am not so sure what happens of merged wet/dry cells
 # CHECK for both u/v and volumes etc.
@@ -151,22 +182,22 @@ u, v, w = velocities
 fill_halo_regions!(u)
 fill_halo_regions!(v)
 fill_halo_regions!(w)
+mask_immersed_field!(v, 0.0)
+mask_immersed_field!(w, 0.0)
+mask_immersed_field!(u, 0.0)
 
-u2, v2, w2 = deepcopy(u), deepcopy(v), deepcopy(w)
-mask_immersed_field!(v2, 0)
-mask_immersed_field!(w2, 0)
-mask_immersed_field!(u2, NaN)
-@assert u == u2
-@assert v == v2
-@assert w == w2
-@info "Velocity fields masked correctly."
+# TODO: Check velocities look reasonable (maybe against tx_trans etc.)
 
-k = Nz
-opt = (; colormap = :RdBu, colorrange = (-1, 1), nan_color = (:black, 1))
-fig, ax, plt = heatmap(view(u2.data, 1:Nx, 1:Ny, k); opt..., axis = (; title = "u at k = $k"))
-plt2 = heatmap(fig[2, 1], view(u2.data, 1:Nx, 1:Ny, k - 1); opt..., axis = (; title = "u at k = $(k - 1)"))
-Label(fig[0, 1], "Near surface u (black = NaNs)", tellwidth = false)
-save(joinpath("output", "surface_u_heatmap.png"), fig)
+# u2, v2, w2 = deepcopy(u), deepcopy(v), deepcopy(w)
+# mask_immersed_field!(v2, NaN)
+# mask_immersed_field!(w2, NaN)
+# mask_immersed_field!(u2, NaN)
+# k = Nz
+# opt = (; colormap = :RdBu, colorrange = (-1, 1), nan_color = (:black, 1))
+# fig, ax, plt = heatmap(view(u2.data, 1:Nx, 1:Ny, k); opt..., axis = (; title = "u at k = $k"))
+# plt2 = heatmap(fig[2, 1], view(u2.data, 1:Nx, 1:Ny, k - 1); opt..., axis = (; title = "u at k = $(k - 1)"))
+# Label(fig[0, 1], "Near surface u (black = NaNs)", tellwidth = false)
+# save(joinpath("output", "surface_u_heatmap.png"), fig)
 
 ################
 # 4. Diffusion #
@@ -183,23 +214,33 @@ model = HydrostaticFreeSurfaceModel(
     grid = grid,
     tracers = :c,
     velocities = PrescribedVelocityFields(; u, v, w),
+    tracer_advection = :RungeKutta3,
     closure = (horizontal_closure, vertical_closure),
     buoyancy = nothing
 )
 
 # Tracer patch for visualization
-Gaussian(λ, φ, L) = exp(-(λ^2 + φ^2) / 2L^2)
-Gaussian(z, Lz) = exp(-(z^2) / 2Lz^2)
+Gaussian(x, x₀, L) = exp(-((x - x₀)^2) / 2L^2)
 
 # Tracer patch parameters
-L = 120 # degree
+Lλ = 2 # degree
+λ₀ = 80 # degrees
+Lφ = 100 # degree
 φ₀ = 0 # degrees
 Lz = 100 # meters
-z₀ = -500 # meters
+z₀ = 0 # meters
 
-cᵢ(λ, φ, z) = Gaussian(λ, φ - φ₀, L) * Gaussian(z - z₀, Lz)
+cᵢ(λ, φ, z) = Gaussian(λ, λ₀, Lλ) * Gaussian(φ, φ₀, Lφ) * Gaussian(z, z₀, Lz)
 
 set!(model, c = cᵢ)
+
+fig = Figure(size = (1920, 1080))
+ax = Axis(fig[1, 1])
+mask = ones(Nx, Ny)
+mask[bottom .== 0] .= NaN
+hm = heatmap!(ax, model.tracers.c.data[1:Nx, 1:Ny, Nz] .* mask; colormap = :RdBu_9, colorrange = (-1, 1))
+Colorbar(fig[1, 2], hm)
+save(joinpath("output", "initial_c_surface.png"), fig)
 
 Δt = 4500 # seconds
 
@@ -235,14 +276,17 @@ simulation.output_writers[:fields] = JLD2Writer(
 
 run!(simulation)
 
-############
-# Plotting #
-############
+# ############
+# # Plotting #
+# ############
 
 c_timeseries = FieldTimeSeries(simulation.output_writers[:fields].filepath, "c")
 cxy_timeseries = FieldTimeSeries(simulation.output_writers[:fields].filepath, "Cxy")
 cyz_timeseries = FieldTimeSeries(simulation.output_writers[:fields].filepath, "Cyz")
 times = c_timeseries.times
+
+land = bottom .== 0
+c_timeseries[land, :, :] .= NaN
 
 set_theme!(Theme(fontsize = 30))
 
@@ -269,7 +313,8 @@ hm = heatmap!(
     # extendhigh = auto,
     # extendlow = auto,
     # colorscale = SymLog(0.01),
-    colormap = :RdBu,
+    colormap = :RdBu_9,
+    nan_color = (:black, 1),
 )
 Colorbar(fig[1, 2], hm)
 
